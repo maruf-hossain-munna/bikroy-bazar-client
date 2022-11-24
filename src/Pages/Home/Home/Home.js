a02b6578fda7478dda906575f6a24eeaaae528af
maruf21hossain <marufhossainmunna21@gmail.com>
2022-11-24 14:16:22 +0600

    Home top banner done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomeTopBanner from './HomeTopBanner';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
+        <div className='container mx-auto'>
+            <HomeTopBanner></HomeTopBanner>
         </div>
     );
 };
